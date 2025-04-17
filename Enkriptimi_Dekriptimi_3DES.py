ce8d1602335979b48a7fd135ffc981ee406e18d7
ErinaMustafa <erina.mustafa@student.uni-pr.edu>
2025-04-17 15:33:13 +0200

    Enkriptimi i tekstit me 3DES

--- Enkriptimi_Dekriptimi_3DES.py
+++ Enkriptimi_Dekriptimi_3DES.py
@@ -4,4 +4,13 @@ from Crypto.Util.Padding import pad, unpad
 
 plaintext = input("Shkruaj tekstin që dëshiron të enkriptosh: ")
 
+key = DES3.adjust_key_parity(get_random_bytes(24))
 
+iv = get_random_bytes(8)
+
+cipher_encrypt = DES3.new(key, DES3.MODE_CBC, iv)
+
+
+ciphertext = cipher_encrypt.encrypt(pad(plaintext.encode(), DES3.block_size))
+
+print("\nTeksti i enkriptuar (hex):", ciphertext.hex())
