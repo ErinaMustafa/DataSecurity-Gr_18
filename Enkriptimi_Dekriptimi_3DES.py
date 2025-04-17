367e143b313af6df61ba7f410cbe0fa8fdda6b96
ErinaMustafa <erina.mustafa@student.uni-pr.edu>
2025-04-17 15:29:55 +0200

    Vendosja e tekstit si input

--- Enkriptimi_Dekriptimi_3DES.py
+++ Enkriptimi_Dekriptimi_3DES.py
@@ -1,3 +1,7 @@
 from Crypto.Cipher import DES3
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
+
+plaintext = input("Shkruaj tekstin që dëshiron të enkriptosh: ")
+
+
