b03b7299ef64580cd84765fee48e84264701275d
eriontroni <142906809+eriontroni@users.noreply.github.com>
2025-04-19 21:35:42 +0200

    update
    Shto krijimin e objektit cipher 3DES dhe dekodimin e inputit Base64

--- dekriptimiFileve.py
+++ dekriptimiFileve.py
@@ -3,4 +3,7 @@ import base64
 key = b'0123456789abcdef'
 def decrypt_text(encrypted_text):
     decrypted_text = ''
+    cipher = DES3.new(key, DES3.MODE_ECB)
+    encrypted_bytes = base64.b64decode(encrypted_text)
+    
     return decrypted_text;
