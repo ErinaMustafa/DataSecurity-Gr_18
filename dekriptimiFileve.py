16639212cd364e6c9dfea485ef4b2c35babfe630
eriontroni <142906809+eriontroni@users.noreply.github.com>
2025-04-19 21:41:19 +0200

    Update tjeter
    Shtimi dekriptimin e bytes dhe
    pastrimin e padding per rikthim ne tekst

--- dekriptimiFileve.py
+++ dekriptimiFileve.py
@@ -5,5 +5,6 @@ def decrypt_text(encrypted_text):
     decrypted_text = ''
     cipher = DES3.new(key, DES3.MODE_ECB)
     encrypted_bytes = base64.b64decode(encrypted_text)
-    
+    decrypted_bytes = cipher.decrypt(encrypted_bytes)
+    decrypted_text = decrypted_bytes.rstrip(b'\x00').decode('utf-8')
     return decrypted_text;
