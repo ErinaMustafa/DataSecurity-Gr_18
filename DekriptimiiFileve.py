28524faf42177f20fe327446ec2516f73c247924
Erisa Sollova <erisasollova@gmail.com>
2025-04-20 00:42:46 +0200

    Krijimi i Funksionit Dekripto

--- DekriptimiiFileve.py
+++ DekriptimiiFileve.py
@@ -13,13 +13,3 @@ def dekripto():
     iv_and_cipher = base64.b64decode(encoded_data)
     iv = iv_and_cipher[:8]
     ciphertext = iv_and_cipher[8:]
-
-    cipher_decrypt = DES3.new(key, DES3.MODE_CBC, iv)
-    decrypted_data = cipher_decrypt.decrypt(ciphertext)
-    decrypted_data = decrypted_data.rstrip(b'\x00')
-
-    with open(output_file, 'wb') as dec_file:
-        dec_file.write(decrypted_data)
-
-    print(f"Fajlli u dekriptua me sukses dhe u ruajt si: {output_file}")
-
