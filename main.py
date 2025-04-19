d90c8a3fef2302ac9ffaad5b329fd954767e22aa
eriontroni <142906809+eriontroni@users.noreply.github.com>
2025-04-19 22:33:21 +0200

    Rregullimi i Main
    Per enkriptim/dekriptim te tekstit duke perdorin celesin shembull

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-import dekriptimiFileve
-import Enkriptimi_Dekriptimi_3DES
+from dekriptimiFileve import decrypt_text
+from Enkriptimi_Dekriptimi_3DES import encrypt_text
 
+key = b'0123456789abcdef' #Celesi universal qe do perdorim si shembull
+while True:
+    prgj = input("Deshironi te enkriptoni(e) apo dekriptoni(d)?\n")
+
+    if(prgj == 'e'):
+        plain_text = input("Shenoni mesazhin per ta enkriptuar: ")
+        encrypted_text = encrypt_text(plain_text)
+        print("Mesazhi juaj i enkriptuar eshte: " + encrypted_text)
+        break;
+    elif(prgj == 'd'):
+        encrypted_text = input("Shenoni tekstin e enkriptuar: ")
+        decrypted_text = decrypt_text(encrypted_text)
+        print("Mesazhi i dekriptuar eshte: " + decrypted_text)
+        break;
+    else:
+        print("Ju lutem jepni pergjigje valide (e ose d)\n")
