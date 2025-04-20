2897e902e069f858b002beee40d08eaf01047c01
eriontroni <142906809+eriontroni@users.noreply.github.com>
2025-04-20 16:00:20 +0200

    Revert "Delete DekriptimiiFileve.py"
    This reverts commit da3deadb11630e8ad7bb7cff97fa562af793955c.

---
+++ DekriptimiiFileve.py
@@ -0,0 +1,27 @@
+from Crypto.Cipher import DES3
+import base64
+import os
+
+key = b'1234567890abcdef12345678'
+
+def dekripto():
+    input_file = input("Shkruaj emrin e fajllit që dëshiron të dekriptosh: ")
+    output_file = input_file.replace(".enc", ".decrypted")
+
+    with open(input_file, 'rb') as enc_file:
+        encoded_data = enc_file.read()
+
+    iv_and_cipher = base64.b64decode(encoded_data)
+    iv = iv_and_cipher[:8]
+    ciphertext = iv_and_cipher[8:]
+
+    cipher_decrypt = DES3.new(key, DES3.MODE_CBC, iv)
+    decrypted_data = cipher_decrypt.decrypt(ciphertext)
+    decrypted_data = decrypted_data.rstrip(b'\x00')
+
+    with open(output_file, 'wb') as dec_file:
+        dec_file.write(decrypted_data)
+
+    print(f" Fajlli u dekriptua me sukses dhe u ruajt si: {output_file}")
+
+
