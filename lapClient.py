88a193c7a09e4e7e1c3abf313a1332a3ec5259a3
ErinaMustafa <erina.mustafa@student.uni-pr.edu>
2025-04-20 16:12:18 +0200

    Modifikim ne lapClient.py

--- lapClient.py
+++ lapClient.py
@@ -4,10 +4,29 @@ import socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
-server_ip = '192.168.1.43'
-server_port = 12345
+server_ip = '192.168.1.43'  # IP e kompjuterit ku është serveri
+server_port = 12345         # Duhet të jetë i njëjtë me atë të serverit
 
-client_socket.connect((server_ip, server_port))
+try:
+    # 3. Lidhja me serverin
+    client_socket.connect((server_ip, server_port))
+    print(f" U lidh me serverin {server_ip}:{server_port}")
 
-message = input("Shkruaj mesazhin qe do dergosh: ")
-client_socket.send(message.encode())
+    while True:
+        # 4. Marrja e mesazhit nga përdoruesi
+        message = input("Shkruaj mesazhin (ose 'dalje' për të dalë): ")
+
+        if message.lower() == 'dalje':
+            print("[x] Po e mbyllim lidhjen.")
+            break
+
+        # 5. Dërgimi i mesazhit te serveri
+        client_socket.send(message.encode())
+
+        # 6. Pritja e përgjigjes nga serveri
+        response = client_socket.recv(1024).decode()
+        print(f" Pergjigje nga serveri: {response}")
+
+except Exception as e:
+    print(f"Ndodhi nje gabim: {str(e)}")
+
