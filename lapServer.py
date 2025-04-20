2eda8f65c43398cc65663144c07b9ff0a051ab5a
eriontroni <142906809+eriontroni@users.noreply.github.com>
2025-04-20 15:37:26 +0200

    lejimi i qasjes se klientit dhe pritja e tij

--- lapServer.py
+++ lapServer.py
@@ -1,3 +1,16 @@
 import socket
 #krijimi i nje serveri socket
 server_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#konffigurimi i ip dhe port
+server_ip = "192.168.1.43"
+server_port = 12345
+
+#lejimi i qasjes se klientit ne server 
+server_socket.bind((server_ip, server_port))
+server_socket.listen(1)
+
+#pritja e klientit per tu qasur
+print(f"Serveri duke pritur lidhje ne portin {server_port} ...")
+
+
